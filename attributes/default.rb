9516a299bc3bffccc8cb5f200ee61ae2d0acaf7f
Richard Shade <rshade@rightscale.com>
2014-10-06 11:53:55 -0500

    changin symbol

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 default['rs-mysql']['dns']['provider'] = 'default'
 default['rs-mysql']['dns']['fog']['provider'] = 'AWS'
-default['rs-mysql']['dns']['credentials'] = {}
+default['rs-mysql']['dns']['credentials'] = []
